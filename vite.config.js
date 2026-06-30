e53c936f2d9bc516cd7477d0819a6f5bed7cc3b6
taoyiping <yiping.tao@changhong.com>
2026-06-30 14:22:40 +0800

    fix: vConsole仅在测试环境注入

--- vite.config.js
+++ vite.config.js
@@ -100,9 +100,11 @@ export default ({ mode }) => {
           data: {
             mode,
             title: '毫米波雷达demo',
-            cdn: [
+            cdn: mode !== 'pro'
+              ? [
                   'https://apppic.mymlsoft.com/app_static/vconsole.min.js',
-                ],
+                ]
+              : [], // 生产构建不注入 vconsole，减小 APK 体积
             env, // 环境变量
           },
         },
